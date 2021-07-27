cbab09fdaa94cf890c1b4bdce72165ce7dc392f7
Elijah Okello <elijahokello90@gmail.com>
2021-07-27 09:23:52 +0300

    funcs

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+// const controllers = require("./lib/commonControllers");
+const dsjwt = require("./lib/DSJwtAuth");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,25 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// Import and initialize the Firebase Admin SDK
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// Function to authenticate the Application
+/**
+@param {req} // Request Object
+@param {res} // Response Object
+*/
+exports.authenticator = functions.https.onRequest((req, res, next) => {
+  dsjwt.login(req, res, next);
+});
+
+// Callback function for Docusign to receive the access token
+exports.callBackJGI = functions.https.onRequest(() => {
+  // receive and store the access token
+});
+
+exports.initiateSigning = functions.https.onRequest((req, res) => {
+  // E signature process initiation
+});
